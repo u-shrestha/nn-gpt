cb67df5d61a7ff30d1338ad5fb2e533b01129152
goodarzi <arash.orca99@gmail.com>
2025-02-08 17:52:34 +0100

    debugs

--- ab/gen/train_n_eval.py
+++ ab/gen/train_n_eval.py
@@ -20,15 +20,15 @@ with open("./conf/config.json") as config_file:
     config = json.load(config_file)
 assert isinstance(config, dict)
 
-token_from_file = bool(config["token_from_file"])
+token_from_file = True if (config["token_from_file"]) == "True" else False
 base_model_name = config["base_model_name"]
 num_epochs = int(config["num_epochs"])
 num_test_epochs = int(config["num_test_epochs"])
-use_deepspeed = bool(config["use_deepspeed"])
+use_deepspeed = True if (config["use_deepspeed"]) == "True" else False
 
 access_token = None
 if token_from_file:
-    with open("./token") as f:
+    with open("../../token") as f:
         access_token = f.readline()
 
 # Deepspeed
