7d2c675597526636bc1f9044c2da9fecb4182e47
ABrain-One <AI-Linux@ABrain.one>
2025-04-01 00:52:35 +0200

    Refactoring

--- ab/gpt/conf/config.py
+++ ab/gpt/conf/config.py
@@ -1,14 +1,12 @@
 # config/config.py
 
-import os
-
 from ab.nn.util.Const import out_dir
 
 # Base directories (adjust as needed)
-DATASET_DESC_DIR = os.path.join(out_dir, "dataset_descriptions")
-GITHUB_REPO_DIR = os.path.join(out_dir, "github_repos")
-FAISS_INDEX_PATH = os.path.join(out_dir, "index", "faiss_index.index")
-FINE_TUNED_MODEL_DIR = os.path.join(out_dir, "fine_tuned_model")
+DATASET_DESC_DIR = out_dir / 'dataset_descriptions'
+GITHUB_REPO_DIR = out_dir / 'github_repos'
+FINE_TUNED_MODEL_DIR = out_dir / 'fine_tuned_model'
+FAISS_INDEX_PATH = str(out_dir / 'index' / 'faiss_index.index')
 
 CODE_EMBEDDING_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
 EMBEDDING_BATCH_SIZE = 8
