bbae6e4f67a0b8a8fb98fba04c29d6b42c1dff35
ABrain-One <AI-Linux@ABrain.one>
2025-04-22 08:23:10 +0200

    Separate fine-tuning pipelines for 1.3 and 7 B LLMs

---
+++ ab/gpt/TuneNNGen_1.3B.py
@@ -0,0 +1,13 @@
+import argparse
+from ab.gpt.util.Tune import tune
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--skip', type=int, default=-1, help='Number of epoches to skip the neural network generation.')
+    parser.add_argument('-p', '--peft', type=str, default=None, help='Path to saved LoRA layers.')
+    args = parser.parse_args()
+    tune(3, 1, args.skip, args.peft, 'NN_gen.json', 'NN_gen.json', 'improve_classification_only',
+         'nngpt-ds-coder_1.3b.json')
+
+if __name__ == '__main__':
+    main()
