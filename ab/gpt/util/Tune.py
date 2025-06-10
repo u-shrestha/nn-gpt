ab73c98f4a1bb656916713047bccb325e162f315
CSXizhang <xizhangsz.0522@gmail.com>
2025-06-10 18:42:13 +0200

    Fix: No sliding window, use Context_length as filter

--- ab/gpt/util/Tune.py
+++ ab/gpt/util/Tune.py
@@ -156,17 +156,21 @@ def tune(test_nn, nn_epoch, skip_epoch, llm_path, llm_tune_conf, nn_gen_conf, co
             nn_gen(out_path, chat_bot, conf_keys, nn_epoch, nn_regenerate_after_exception, prompt_dict, test_nn, model_loader.get_max_length(), always_save_full_output=always_save_full_output)
         # fine tune model for 1 epoch / Using training_args and save copy
         print(f'[DEBUG]Perform finetune at epoch {epoch}.')
-        data_processor = NNGenPrompt(model_loader.get_max_length(), tokenizer, train_config_path)
+        # data_processor = NNGenPrompt(model_loader.get_max_length(), tokenizer, train_config_path)
+        if context_length:
+            data_processor = NNGenPrompt(context_length, tokenizer, train_config_path)
+        else:
+            data_processor = NNGenPrompt(model_loader.get_max_length(), tokenizer, train_config_path)
         dataset = data_processor.get_dataset(only_best_accuracy, n_training_prompts=n_training_prompts)
         # dataset = load_from_disk(nngpt_dir / 'dataset')
 
-        if context_length:
-            chunked_dataset = dataset.map(
-                lambda x: apply_sliding_window(x, context_length, 1024, tokenizer),
-                remove_columns=dataset.column_names,
-                batch_size=16
-            )
-            dataset = chunked_dataset.map(flatten_chunks, batched=True, remove_columns=["chunks"])
+        # if context_length:
+        #     chunked_dataset = dataset.map(
+        #         lambda x: apply_sliding_window(x, context_length, 1024, tokenizer),
+        #         remove_columns=dataset.column_names,
+        #         batch_size=16
+        #     )
+        #     dataset = chunked_dataset.map(flatten_chunks, batched=True, remove_columns=["chunks"])
 
         # print('Dataset length:', len(dataset))
         print('Dataset length:', len(dataset))
