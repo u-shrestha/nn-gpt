5f48c9adb92c93a208fc6d3f828b953f7b87a726
ABrain-One <AI-Linux@ABrain.one>
2025-08-17 17:26:34 +0200

    Qwen3 Coder

---
+++ ab/gpt/NNAlter_30B_code_qw.py
@@ -0,0 +1,14 @@
+import argparse
+
+from ab.gpt.util.AlterNN import alter
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--epochs', type=int, default=8, help="Maximum number of generation epochs.")
+    args = parser.parse_args()
+    alter(args.epochs, 'NN_alter.json', 'Qwen/Qwen3-Coder-30B-A3B-Instruct-FP8')
+
+
+if __name__ == "__main__":
+    main()
