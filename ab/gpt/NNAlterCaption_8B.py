156109c40a2337d8d4663a73a6b9f548355872a0
KrunalJesani <krunaljesani@gmail.com>
2025-09-22 12:06:33 +0200

    R1-Qwen3-8B

--- ab/gpt/NNAlterCaption_8B.py
+++ ab/gpt/NNAlterCaption_8B.py
@@ -1,7 +1,7 @@
 import argparse
 from pathlib import Path
 
-from ab.gpt.util.AlterCaptionNN import alter
+from ab.gpt.util.AlterCaptionNNtune import alter
 from ab.gpt.util.Const import conf_test_dir
 
 
@@ -10,7 +10,7 @@ def main():
     parser.add_argument("-e", "--epochs", type=int, default=5, help="Number of generation epochs to run.")
     parser.add_argument("-c", "--conf", type=str, default="NN_Caption_master.json")
     parser.add_argument("-nn", type=str, default=None, help="Generate variants ONLY for this model (e.g. RESNETLSTM, ResNetTransformer) otherwise default all.")
-    parser.add_argument("-m", "--model", type=str, default="deepseek-ai/DeepSeek-R1-0528-Qwen3-8B", help="HF model id or local model path.")
+    parser.add_argument("-m", "--model", type=str, default="deepseek-ai/DeepSeek-R1-0528-Qwen3-8B", help="HF model id or local model path.Choose a smaller model for faster generation.")
     parser.add_argument("--gguf", type=str, default=None, help="Optional GGUF file path if using a local/llama.cpp backend.")
 
     args = parser.parse_args()
