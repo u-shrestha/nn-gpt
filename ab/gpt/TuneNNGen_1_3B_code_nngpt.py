073a77156e4086a6d68ac727834c9ea70c98c511
ABrain-One <AI-Linux@ABrain.one>
2025-10-03 14:17:09 +0200

    Two versions for nngpt_ds_coder_1.3b_instruct.json and ds_coder_1.3b_instruct.json

--- ab/gpt/TuneNNGen_1_3B_code_nngpt.py
+++ ab/gpt/TuneNNGen_1_3B_code_nngpt.py
@@ -2,7 +2,7 @@ import ab.gpt.TuneNNGen as TuneNNGen
 
 
 def main():
-    TuneNNGen.main(llm_conf='ds_coder_1.3b_instruct.json', max_new_tokens=12 * 1024)
+    TuneNNGen.main(llm_conf='nngpt_ds_coder_1.3b_instruct.json', max_new_tokens=12 * 1024)
 
 
 if __name__ == '__main__':
