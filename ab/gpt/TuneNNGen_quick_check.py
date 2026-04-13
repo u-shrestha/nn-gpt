d3ff67cba98e3c805f2b539ab014a9c7fb53ed62
ABrain-One <AI-Linux@ABrain.one>
2026-04-13 17:42:15 +0200

    For a quick check of the full pipeline

--- ab/gpt/TuneNNGen_quick_check.py
+++ ab/gpt/TuneNNGen_quick_check.py
@@ -7,7 +7,7 @@ def main():
                    test_nn=2,
                    skip_epoches=0,
                    nn_name_prefix='check_results',
-                   unsloth_opt=True
+                   unsloth_opt=False
                    )
 
 
